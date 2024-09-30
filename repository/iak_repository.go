4293014e84d78c38027c25aff2be0f76f91c41f4
Mac <mac@MacBook-Pro-Mac.local>
2024-10-01 00:19:18 +0700

    feat: add

--- repository/iak_repository.go
+++ repository/iak_repository.go
@@ -38,7 +38,8 @@ func (*iakApiRepository) IakTopUpPayRepository(payload *model.PrePaidIakBody) (*
 
 	payload.Sign = sign(payload.RefID)
 
-	resp, err := doRequestIak(http.MethodPost, "https://prepaid.iak.dev/api/top-up", payload)
+	// resp, err := doRequestIak(http.MethodPost, "https://prepaid.iak.dev/api/top-up", payload)
+	resp, err := doRequestIak(http.MethodPost, "https://prepaid.iak.id/api/top-up", payload)
 
 	if err != nil {
 		return nil, err
@@ -240,7 +241,8 @@ func (*iakApiRepository) ElectricityTokenInquiryRepository(payload *model.PrePai
 	payload.Username = config.AppConfig.UsernameIak
 	payload.Sign = sign(payload.CustomerID)
 
-	resp, err := doRequestIak(http.MethodPost, "https://prepaid.iak.dev/api/inquiry-pln", payload)
+	// resp, err := doRequestIak(http.MethodPost, "https://prepaid.iak.dev/api/inquiry-pln", payload)
+	resp, err := doRequestIak(http.MethodPost, "https://prepaid.iak.id/api/inquiry-pln", payload)
 
 	if err != nil {
 		return nil, err
